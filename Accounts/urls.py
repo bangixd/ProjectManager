2afbc6038344e62ac2e97f8b4b882be29663423a
Afshin Moussavi <khodeafshin666@gmail.com>
2024-11-24 16:18:32 +0330

    Add log out view and jwt to Accounts model

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenBlacklistView,
+    TokenVerifyView
 )
 app_name = 'accounts'
 
@@ -13,9 +14,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('create/user/', views.UserCreate.as_view(), name='create_user'),
     path('profile/', views.UserProfileDetail.as_view(), name='user_profile'),
     path('user/logout/', views.UserLogoutViews.as_view(), name='user_logout'),
 
 ]
-
