115008caaaab364af01fce3601943d79b0502aed
Afshin Moussavi <khodeafshin666@gmail.com>
2024-11-24 17:28:59 +0330

    Add password change view to Accounts

--- ProjectManager/settings.py
+++ ProjectManager/settings.py
@@ -168,3 +168,11 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),  
 }
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tezarafshin@gmail.com'
+EMAIL_HOST_PASSWORD = 'fnwz nkza riev yycd'
+DEFAULT_FROM_EMAIL = 'tezarafshin@gmail.com'
