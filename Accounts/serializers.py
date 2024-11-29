95255c5b9f93172fafc7444448aee38f7300530b
Afshin Moussavi <khodeafshin666@gmail.com>
2024-11-30 00:19:27 +0330

    Add swagger and Accounts app docs

--- Accounts/serializers.py
+++ Accounts/serializers.py
@@ -8,6 +8,15 @@ from ProjectManager.custom_validator import CustomPasswordValidator
 from django.contrib.auth import password_validation
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user creation and management.
+
+    - Fields: Includes basic user information such as first name, last name, email, role, phone number, and password.
+    - Password: Write-only field; validated using custom and Django validators.
+    - VIP status: Read-only field.
+    - Automatically hashes the password upon user creation.
+    """
+    
     class Meta:
         model = User
         fields = [
@@ -44,12 +53,26 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user profile.
+
+    - Fields: Includes the profile image of the user.
+    """
+    
     class Meta:
         model = Profile
         fields = ['image']
     
 
 class UserProfileDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializer for detailed user profile view.
+
+    - Combines data from the user and profile models.
+    - Fields: Includes user’s name, email, role, phone number, VIP status, and profile image.
+    - Read-only for all fields.
+    """
+    
     first_name = serializers.CharField(source='user.first_name')
     last_name = serializers.CharField(source='user.last_name')
     email = serializers.EmailField(source='user.email')
@@ -74,6 +97,13 @@ class UserProfileDetailSerializer(serializers.ModelSerializer):
 
 
 class UserLogoutSerializer(serializers.Serializer):
+    """
+    Serializer for user logout.
+
+    - Validates and processes refresh tokens to blacklist them.
+    - Raises an error if the token is invalid or already blacklisted.
+    """
+    
     refresh = serializers.CharField(required=True)
 
     def validate(self, attrs):
@@ -85,10 +115,17 @@ class UserLogoutSerializer(serializers.Serializer):
             refresh_token = RefreshToken(self.token)
             refresh_token.blacklist()
         except Exception as e:
-            raise ValidationError("Invalid token or token already blacklisted")
+            raise serializers.ValidationError("Invalid token or token already blacklisted")
     
 
 class ChangePasswordSerializers(serializers.Serializer):
+    """
+    Serializer for changing the user password.
+
+    - Fields: Includes old password, new password, and confirm password.
+    - Validations: Ensures the old password matches the user’s current password and new passwords are valid and match.
+    """
+    
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
     confirm_password = serializers.CharField(required=True)
@@ -105,10 +142,25 @@ class ChangePasswordSerializers(serializers.Serializer):
 
 
 class PasswordResetRequestSerializer(serializers.Serializer):
+    """
+    Serializer for requesting a password reset.
+
+    - Field: Includes user email to initiate the password reset process.
+    - Sends a password reset link to the provided email.
+    """
+    
     email = serializers.EmailField(required=True)
 
 
 class PasswordResetConfirmSerializer(serializers.Serializer):
+    """
+    Serializer for confirming password reset.
+
+    - Fields: Includes new password and confirm password.
+    - Validations: Ensures new passwords are valid and match.
+    - Saves the new password for the user upon successful validation.
+    """
+    
     new_password = serializers.CharField(write_only=True, required=True)
     confirm_password = serializers.CharField(write_only=True, required=True)
 
