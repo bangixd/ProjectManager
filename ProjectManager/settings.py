11ab9ab67bc460153ea5c3c0866ae16892a93903
Afshin Moussavi <khodeafshin666@gmail.com>
2024-12-03 00:30:04 +0330

    config postgreSQL

--- ProjectManager/settings.py
+++ ProjectManager/settings.py
@@ -86,12 +86,30 @@ WSGI_APPLICATION = 'ProjectManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES = {
+        "default": {
+            "ENGINE": 'django.db.backends.postgresql',
+            "NAME": 'Project_Manager',
+            "USER": 'postgres',
+            "PASSWORD": '1234',
+            "HOST": '127.0.0.1',
+            "PORT": '5432'
+        }
+    }
 
 
 # Password validation
